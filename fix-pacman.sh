4f90166e7e8649a7a87ee9a659dbe32e1688b7a7
EVIKA2732 <mbernet350@gmail.com>
2019-10-28 22:28:43 +0100

    Create fix-pacman.sh

---
+++ fix-pacman.sh
@@ -0,0 +1,5 @@
+cat /etc/passwd | awk -F: '{print $ 1}' && sudo passwd && rm -f -v -r /var/lib/pacman/db.lck && 
+
+locate -e --regex "\.pac(new|save|orig)$"
+
+&& sudo pacman -S archlinux-keyring && sudo updatedb && trizen -Rs kalu && trizen -S pamac-classic && pacman -Suyy
